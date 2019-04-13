f52a2d86f8e8faeb011c0eb92b966e54df9c172a
Geovane Carvalho <geovaneoliveira.75@hotmail.com>
2019-04-13 04:08:31 -0300

    contato config

--- Geotec/Form1.Designer.cs
+++ Geotec/Form1.Designer.cs
@@ -31,23 +31,23 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmGeotec));
             this.label2 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.txtDescricao = new System.Windows.Forms.RichTextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.lblNome = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.btnGarantia = new System.Windows.Forms.Button();
-            this.btnBackup = new System.Windows.Forms.Button();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.btnWhatsapp = new System.Windows.Forms.Button();
-            this.btnMensagem = new System.Windows.Forms.Button();
             this.btnSite = new System.Windows.Forms.Button();
+            this.btnMensagem = new System.Windows.Forms.Button();
+            this.btnWhatsapp = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnBackup = new System.Windows.Forms.Button();
+            this.btnGarantia = new System.Windows.Forms.Button();
+            this.txtDescricao = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.btnSair = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label2
@@ -74,53 +74,46 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Bem-Vindo ao nosso formulário";
             // 
-            // pictureBox1
+            // groupBox3
             // 
-            this.pictureBox1.ErrorImage = null;
-            this.pictureBox1.Image = global::Geotec.Properties.Resources.geo;
-            this.pictureBox1.Location = new System.Drawing.Point(-1, -20);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(585, 278);
-            this.pictureBox1.TabIndex = 3;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.groupBox3.Controls.Add(this.btnSite);
+            this.groupBox3.Controls.Add(this.btnMensagem);
+            this.groupBox3.Controls.Add(this.btnWhatsapp);
+            this.groupBox3.Location = new System.Drawing.Point(6, 231);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(550, 73);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Qualquer dúvida fale conosco clicando nos butões abaixo";
             // 
-            // txtDescricao
+            // btnSite
             // 
-            this.txtDescricao.Enabled = false;
-            this.txtDescricao.Location = new System.Drawing.Point(6, 19);
-            this.txtDescricao.Name = "txtDescricao";
-            this.txtDescricao.Size = new System.Drawing.Size(532, 96);
-            this.txtDescricao.TabIndex = 0;
-            this.txtDescricao.Text = "Olá fulano de tal obrigado por fazer este serviço conosco\n\n";
+            this.btnSite.Location = new System.Drawing.Point(6, 24);
+            this.btnSite.Name = "btnSite";
+            this.btnSite.Size = new System.Drawing.Size(174, 36);
+            this.btnSite.TabIndex = 5;
+            this.btnSite.Text = "Site";
+            this.btnSite.UseVisualStyleBackColor = true;
+            this.btnSite.Click += new System.EventHandler(this.btnSite_Click);
             // 
-            // label1
+            // btnMensagem
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(16, 29);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(26, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Olá ";
+            this.btnMensagem.Location = new System.Drawing.Point(368, 24);
+            this.btnMensagem.Name = "btnMensagem";
+            this.btnMensagem.Size = new System.Drawing.Size(176, 36);
+            this.btnMensagem.TabIndex = 4;
+            this.btnMensagem.Text = "Enviar Mensagem";
+            this.btnMensagem.UseVisualStyleBackColor = true;
             // 
-            // lblNome
+            // btnWhatsapp
             // 
-            this.lblNome.AutoSize = true;
-            this.lblNome.Location = new System.Drawing.Point(37, 29);
-            this.lblNome.Name = "lblNome";
-            this.lblNome.Size = new System.Drawing.Size(72, 13);
-            this.lblNome.TabIndex = 2;
-            this.lblNome.Text = "Fulano de Tal";
-            this.lblNome.Click += new System.EventHandler(this.lblNome_Click);
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(339, 53);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(215, 13);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "É um prazer ter você como cliente obrigado.";
+            this.btnWhatsapp.Location = new System.Drawing.Point(186, 24);
+            this.btnWhatsapp.Name = "btnWhatsapp";
+            this.btnWhatsapp.Size = new System.Drawing.Size(176, 36);
+            this.btnWhatsapp.TabIndex = 3;
+            this.btnWhatsapp.Text = "WhatsApp Web";
+            this.btnWhatsapp.UseVisualStyleBackColor = true;
+            this.btnWhatsapp.Click += new System.EventHandler(this.btnWhatsapp_Click);
             // 
             // groupBox2
             // 
@@ -134,15 +127,6 @@
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Descrição do Serviço";
             // 
-            // btnGarantia
-            // 
-            this.btnGarantia.Location = new System.Drawing.Point(274, 121);
-            this.btnGarantia.Name = "btnGarantia";
-            this.btnGarantia.Size = new System.Drawing.Size(129, 23);
-            this.btnGarantia.TabIndex = 1;
-            this.btnGarantia.Text = "Sua garantia";
-            this.btnGarantia.UseVisualStyleBackColor = true;
-            // 
             // btnBackup
             // 
             this.btnBackup.Location = new System.Drawing.Point(409, 121);
@@ -151,45 +135,66 @@
             this.btnBackup.TabIndex = 2;
             this.btnBackup.Text = "Backup ";
             this.btnBackup.UseVisualStyleBackColor = true;
+            this.btnBackup.Click += new System.EventHandler(this.btnBackup_Click);
             // 
-            // groupBox3
+            // btnGarantia
             // 
-            this.groupBox3.Controls.Add(this.btnSite);
-            this.groupBox3.Controls.Add(this.btnMensagem);
-            this.groupBox3.Controls.Add(this.btnWhatsapp);
-            this.groupBox3.Location = new System.Drawing.Point(6, 231);
-            this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(550, 73);
-            this.groupBox3.TabIndex = 5;
-            this.groupBox3.TabStop = false;
-            this.groupBox3.Text = "Qualquer dúvida fale conosco clicando nos butões abaixo";
+            this.btnGarantia.Location = new System.Drawing.Point(274, 121);
+            this.btnGarantia.Name = "btnGarantia";
+            this.btnGarantia.Size = new System.Drawing.Size(129, 23);
+            this.btnGarantia.TabIndex = 1;
+            this.btnGarantia.Text = "Sua garantia";
+            this.btnGarantia.UseVisualStyleBackColor = true;
+            this.btnGarantia.Click += new System.EventHandler(this.btnGarantia_Click);
             // 
-            // btnWhatsapp
+            // txtDescricao
             // 
-            this.btnWhatsapp.Location = new System.Drawing.Point(186, 24);
-            this.btnWhatsapp.Name = "btnWhatsapp";
-            this.btnWhatsapp.Size = new System.Drawing.Size(176, 36);
-            this.btnWhatsapp.TabIndex = 3;
-            this.btnWhatsapp.Text = "WhatsApp Web";
-            this.btnWhatsapp.UseVisualStyleBackColor = true;
+            this.txtDescricao.Enabled = false;
+            this.txtDescricao.Location = new System.Drawing.Point(6, 19);
+            this.txtDescricao.Name = "txtDescricao";
+            this.txtDescricao.Size = new System.Drawing.Size(532, 96);
+            this.txtDescricao.TabIndex = 0;
+            this.txtDescricao.Text = "Olá fulano de tal obrigado por fazer este serviço conosco\n\n";
             // 
-            // btnMensagem
+            // label4
             // 
-            this.btnMensagem.Location = new System.Drawing.Point(368, 24);
-            this.btnMensagem.Name = "btnMensagem";
-            this.btnMensagem.Size = new System.Drawing.Size(176, 36);
-            this.btnMensagem.TabIndex = 4;
-            this.btnMensagem.Text = "Enviar Mensagem";
-            this.btnMensagem.UseVisualStyleBackColor = true;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(313, 53);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(243, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "É um prazer ter você como cliente muito obrigado.";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
-            // btnSite
+            // lblNome
             // 
-            this.btnSite.Location = new System.Drawing.Point(6, 24);
-            this.btnSite.Name = "btnSite";
-            this.btnSite.Size = new System.Drawing.Size(174, 36);
-            this.btnSite.TabIndex = 5;
-            this.btnSite.Text = "Site";
-            this.btnSite.UseVisualStyleBackColor = true;
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(37, 29);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(72, 13);
+            this.lblNome.TabIndex = 2;
+            this.lblNome.Text = "Fulano de Tal";
+            this.lblNome.Click += new System.EventHandler(this.lblNome_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Olá ";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.Image = global::Geotec.Properties.Resources.geo;
+            this.pictureBox1.Location = new System.Drawing.Point(-1, -20);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(585, 278);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // btnSair
             // 
@@ -215,13 +220,13 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmGeotec";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "GeoTec Informática";
+            this.Text = ",";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.groupBox2.ResumeLayout(false);
             this.groupBox3.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- Geotec/Form1.cs
+++ Geotec/Form1.cs
@@ -20,7 +20,11 @@ namespace Geotec
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            string txt = System.IO.File.ReadAllText(@"C:\tecinfo\nome.txt");
+            lblNome.Text = txt;
 
+            string txtDes = System.IO.File.ReadAllText(@"C:\tecinfo\desc.txt");
+            txtDescricao.Text = txtDes;
         }
 
         private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
@@ -57,6 +61,43 @@ namespace Geotec
         private void lblNome_Click(object sender, EventArgs e)
         {
             
+        }
+
+        private void btnGarantia_Click(object sender, EventArgs e)
+        {
+            string data = System.IO.File.ReadAllText(@"C:\tecinfo\data.txt");
+
+            DateTime dataAtual = DateTime.Now.Date;
+            DateTime outraData = Convert.ToDateTime(data);
+
+            if (dataAtual > outraData || dataAtual == outraData)
+            {
+                MessageBox.Show("Sua Garantia expirou o prazo!");
+            }
+            else
+            {
+                MessageBox.Show(data, "Sua Garantia é valida até o dia!");
+            }
+        }
+
+        private void btnSite_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://geotecinformatica.herokuapp.com/");
+        }
+
+        private void btnWhatsapp_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://wa.me/5561995488068?text=Olá%20Geovane%20Carvalho!");
+        }
+
+        private void btnBackup_Click(object sender, EventArgs e)
+        {
+            Process.Start("D:/bkp");
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
